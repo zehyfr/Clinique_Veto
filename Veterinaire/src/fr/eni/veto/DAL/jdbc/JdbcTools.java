f66325b4b9bb9030a5467ddde783f8461645cf65
abrochard2017 <abrochard2017@3-UC31-19.ad.campus-eni.fr>
2018-03-08 09:31:53 +0100

    JDbc Tools

--- Veterinaire/src/fr/eni/veto/DAL/jdbc/JdbcTools.java
+++ Veterinaire/src/fr/eni/veto/DAL/jdbc/JdbcTools.java
@@ -5,8 +5,6 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-import com.microsoft.sqlserver.jdbc.SQLServerDriver;
-
 import fr.eni.veto.DAL.Settings;
 
 public class JdbcTools {
@@ -28,9 +26,7 @@ public class JdbcTools {
 	}
 	
 	public static Connection getConnection() throws SQLException{
-		Connection c = null;
-		DriverManager.registerDriver(new SQLServerDriver());
-		c = (DriverManager.getConnection(urldb, userdb, passworddb));
+		Connection c = DriverManager.getConnection(urldb, userdb, passworddb);
 		return c;
 	}
 	
