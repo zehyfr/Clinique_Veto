dc8316b728e4b8126f45d9e71dff3abf03ddbb11
lrampillon2017 <lrampillon2017@3-UC31-16.ad.campus-eni.fr>
2018-03-15 17:07:03 +0100

    codePersonnel

--- Veterinaire/src/fr/eni/veto/BLL/AgendaMger.java
+++ Veterinaire/src/fr/eni/veto/BLL/AgendaMger.java
@@ -1,30 +1,30 @@
 package fr.eni.veto.BLL;
 
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Date;
 
 import fr.eni.veto.BO.Agendas;
+import fr.eni.veto.DAL.AgendaDAO;
+import fr.eni.veto.DAL.AgendaDAOFactory;
 import fr.eni.veto.DAL.DALException;
-import fr.eni.veto.DAL.agenda.AgendaDAOImpl;
 
 public class AgendaMger {
 	
 	public void nouveauRDV(Agendas agendas) throws DALException
 	{
-		AgendaDAOImpl insertAnimal = new AgendaDAOImpl();
+		AgendaDAO insertAnimal = AgendaDAOFactory.getAgendaDAO();
 		insertAnimal.ajouterRdv(agendas);
 	}
 	
 	public ArrayList<Agendas> getRDV(int aVeto, Date aDate) throws DALException
 	{
-		AgendaDAOImpl getList = new AgendaDAOImpl();
+		AgendaDAO getList = AgendaDAOFactory.getAgendaDAO();
 		return getList.selectAll(aVeto, aDate);
 	}
 	
-	public int verify(int aCode, Date aDate) throws DALException, SQLException
+	public int verify(int aCode, Date aDate) throws DALException
 	{
-		AgendaDAOImpl agendaVerif = new AgendaDAOImpl();
+		AgendaDAO agendaVerif = AgendaDAOFactory.getAgendaDAO();
 		return agendaVerif.verify(aCode, aDate);
 	}
 }
